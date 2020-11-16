61cee334eda6e026df7ca1967bf125f54c3558b5
Joel Rdz <joelrdzh@gmail.com>
2020-11-16 11:37:23 -0600

    Reacting to Params Changes

---
+++ 45-vue-router-reacting/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+<div id="app">
+	<h1>Vue Router - Reacting to Params Changes</h1>
+	<h1>Rutas con Vue Router</h1>
+		<router-link to="/" exact active-class="activo" tag="li">
+			<a>Home</a>
+		</router-link>
+		<router-link to="/contacto" active-class="activo" tag="li" >
+			<a>Contacto</a>
+		</router-link>
+		<router-link to="/usuario/77" active-class="activo" tag="li" >
+			<a>Usuario 77</a>
+		</router-link>
+		<router-link to="/usuario/99" active-class="activo" tag="li" >
+			<a>Usuario 99</a>
+		</router-link>
+		<hr>
+	<router-view></router-view>
+</div>
+</template>
+
+<script>
+export default {
+	name: 'App',
+	components: {}
+}
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+
+.activo a {
+	color: red;
+}
+</style>
