332904b8892074ad48fa856c45b77d97cb265f15
Joel Rdz <joelrdzh@gmail.com>
2020-11-16 19:07:39 -0600

    Vou router - Nested nav

---
+++ 47-vue-router-nested-nav/src/routes.js
@@ -0,0 +1,21 @@
+import Home from './components/Home'
+import Equipo from './components/Equipo'
+import Usuario from './components/Usuario'
+import UsuarioBio from './components/UsuarioBio'
+import UsuarioFotos from './components/UsuarioFotos'
+
+export const routes = [
+	{ path: '/', component: Home },
+	{ path: '/equipo/:id',
+		component: Equipo,
+		children: [
+			{ path: '',
+				component: Usuario,
+				children: [
+					{ path: 'bio', component: UsuarioBio },
+					{ path: 'fotos', component: UsuarioFotos }
+				]
+			}
+		]
+	}
+]
