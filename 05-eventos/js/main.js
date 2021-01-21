853552e21cc55443319f8ec535cd42389f19ea7d
Joel Rdz <joelrdzh@gmail.com>
2021-01-21 03:32:38 -0600

    Eventos

---
+++ 05-eventos/js/main.js
@@ -0,0 +1,25 @@
+const vm = new Vue({
+  el: 'main',
+  data: {
+    newTodo: null,
+    todos: [
+      'Clean playlists',
+      'Search movies',
+      'Empty trash'
+    ]
+  },
+  methods: {
+    addTodo() {
+      this.todos.unshift(this.newTodo);
+      this.newTodo = '';
+    }
+  }
+});
+
+// Vanilla JavaScript
+// function addTodo() {
+//   console.log('a')
+//   const input = document.querySelector('input[type=text]');
+//   vm.todos.unshift(input.value);
+//   input.value = '';
+// }
