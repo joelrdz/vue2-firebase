9778abced25423f30b35129e1bb68f47963eb25b
Joel Rdz <joelrdzh@gmail.com>
2021-01-26 15:30:45 -0600

    Transiciones y animaciones

---
+++ 10-transiciones-animaciones/js/main.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: 'main',
+  data: {
+    show: true,
+    messages: {
+      transition: 'Transiciones CSS con Vue.js',
+      animation: 'Animaciones CSS con Vue.js',
+      animationCustom: 'Animaciones custom CSS con Vue.js',
+      betweenElements: 'Transiciones entre elementos con Vue.js'
+    }
+  }
+});
